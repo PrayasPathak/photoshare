641ca66b34ccc667eb4248fa5a3bde744b610650
PrayasPathak <prayasroman1@gmail.com>
2024-01-27 07:36:55 +0545

    Added form to add image and filter images by category.

--- photos/views.py
+++ photos/views.py
@@ -1,13 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 # App level imports
 from .models import Category, Photo
+import PIL
 
 
 # Create your views here.
 def gallery(request):
     categories = Category.objects.all()
-    photos = Photo.objects.all()
+    category = request.GET.get("category")
+    if category == None:
+        photos = Photo.objects.all()
+    else:
+        photos = Photo.objects.filter(category__name=category)
     context = {
         "categories": categories,
         "photos": photos,
@@ -16,7 +22,34 @@ def gallery(request):
 
 
 def addPhoto(request):
-    return render(request, "photos/add_photo.html")
+    categories = Category.objects.all()
+    if request.method == "POST":
+        data = request.POST
+        image = request.FILES["image"]
+        if data["category"] != "none":
+            category = Category.objects.get(id=data["category"])
+        elif data["category_new"] != "":
+            category, created = Category.objects.get_or_create(
+                name=data["category_new"]
+            )
+        else:
+            category = None
+
+        # Creating an image
+        try:
+            Photo.objects.create(
+                category=category, description=data["description"], image=image
+            )
+            messages.success(request, "Photo was added successfully.")
+            return redirect("gallery")
+        except PIL.UnidentifiedImageError:
+            messages(
+                request,
+                "An error occurred while adding the image. Please make sure the image is not corrupt.",
+            )
+            return redirect("add")
+    context = {"categories": categories}
+    return render(request, "photos/add_photo.html", context)
 
 
 def viewPhoto(request, pk):
