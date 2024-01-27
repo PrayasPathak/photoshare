51396f39988736a060938ed62452384e0f40541d
PrayasPathak <prayasroman1@gmail.com>
2024-01-27 06:57:33 +0545

    improved template for gallery.html and photo.html for dynamic content rendering.

--- photos/views.py
+++ photos/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 
+# App level imports
+from .models import Category, Photo
+
 
 # Create your views here.
 def gallery(request):
-    return render(request, "photos/gallery.html")
+    categories = Category.objects.all()
+    photos = Photo.objects.all()
+    context = {
+        "categories": categories,
+        "photos": photos,
+    }
+    return render(request, "photos/gallery.html", context)
 
 
 def addPhoto(request):
@@ -11,4 +20,6 @@ def addPhoto(request):
 
 
 def viewPhoto(request, pk):
-    return render(request, "photos/photo.html")
+    photo = Photo.objects.get(id=pk)
+    context = {"photo": photo}
+    return render(request, "photos/photo.html", context)
