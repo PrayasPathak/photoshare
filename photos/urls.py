25f40c97afc66683b77f9ca7b8cc3af91a5f26b0
PrayasPathak <prayasroman1@gmail.com>
2024-04-06 14:06:37 +0545

    Registration, login and logout functionlity

--- photos/urls.py
+++ photos/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("login/", views.login_user, name="login"),
+    path("logout/", views.logout_user, name="logout"),
+    path("register/", views.register_user, name="register"),
     path("", views.gallery, name="gallery"),
     path("photo/<str:pk>/", views.viewPhoto, name="photo"),
     path("add/", views.addPhoto, name="add"),
--- photos/views.py
+++ photos/views.py
@@ -1,19 +1,58 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
 
 # App level imports
 from .models import Category, Photo
+from .forms import CustomUserCreationForm
 import PIL
 
 
 # Create your views here.
+def login_user(request):
+    if request.user.is_authenticated:
+        return redirect("gallery")
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("gallery")
+    page = "login"
+    return render(request, "photos/login_register.html", context={"page": page})
+
+
+def logout_user(request):
+    logout(request)
+    return redirect("login")
+
+
+def register_user(request):
+    page = "register"
+    form = CustomUserCreationForm()
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.save()
+            if user is not None:
+                login(request, user)
+                return redirect("gallery")
+    context = {"form": form, "page": page}
+    return render(request, "photos/login_register.html", context)
+
+
+@login_required(login_url="login")
 def gallery(request):
-    categories = Category.objects.all()
+    user = request.user
+    categories = Category.objects.filter(user=user)
     category = request.GET.get("category")
     if category == None:
-        photos = Photo.objects.all()
+        photos = Photo.objects.filter(category__user=user)
     else:
-        photos = Photo.objects.filter(category__name=category)
+        photos = Photo.objects.filter(category__name=category, category__user=user)
     context = {
         "categories": categories,
         "photos": photos,
@@ -21,8 +60,10 @@ def gallery(request):
     return render(request, "photos/gallery.html", context)
 
 
+@login_required(login_url="login")
 def addPhoto(request):
-    categories = Category.objects.all()
+    user = request.user
+    categories = user.category_set.all()
     if request.method == "POST":
         data = request.POST
         image = request.FILES["image"]
@@ -30,7 +71,7 @@ def addPhoto(request):
             category = Category.objects.get(id=data["category"])
         elif data["category_new"] != "":
             category, created = Category.objects.get_or_create(
-                name=data["category_new"]
+                user=user, name=data["category_new"]
             )
         else:
             category = None
@@ -52,6 +93,7 @@ def addPhoto(request):
     return render(request, "photos/add_photo.html", context)
 
 
+@login_required(login_url="login")
 def viewPhoto(request, pk):
     photo = Photo.objects.get(id=pk)
     context = {"photo": photo}
